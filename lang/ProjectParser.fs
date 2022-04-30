e537d86dd5b940b427c12742219479af983c5a58
Valeria Starkova <stavaser@icloud.com>
2022-04-29 21:35:00 -0400

    kinda working for bar?

--- lang/ProjectParser.fs
+++ lang/ProjectParser.fs
@@ -1,88 +1,111 @@
 module ProjectParser
 
 open FParsec
-type Num = int
+// type Num = int
 
 type Note =
-    | Num
+    | Num of uint8
     | E
     | And
     | A
 
-type VarName = string
+type PatternName = string
+type BarName = string
 
-type Pattern = Pattern of VarName * (string list)
-
+type Pattern = Pattern of PatternName * (Note list)
 
 type Drum =
-    | HH of string
-    | SN of string
-    | BD of string
+    | HH
+    | SN
+    | BD
 
-type Bar = Bar of VarName * ((Drum * Pattern) list)
+// type DrumPattern =
+//     | DrumPatternVar of Drum * PatternName
+//     | DrumPatternNotes of Drum * (Note list)
+
+type Bar = Bar of BarName * (((Drum * PatternName) * (Drum * (Note list))) list)
 // type Expr =
 // parse notes
 
 let ws0 = spaces
 let ws1 = spaces1
 
-let num: Parser<Note, unit> = (digit .>> ws0) |>> (fun e -> Num)
+let str s = pstring s
+let str_ws0 s = pstring s .>> ws0
+let str_ws1 s = pstring s .>> ws1
+
+
+let num: Parser<Note, unit> = (puint8 .>> ws0) |>> (fun e -> Num(e))
 let e: Parser<Note, unit> = (pchar 'e' .>> ws0) |>> (fun e -> E)
 let and': Parser<Note, unit> = (pchar '+' .>> ws0) |>> (fun e -> And)
 let a: Parser<Note, unit> = (pchar 'a' .>> ws0) |>> (fun e -> A)
 // let and' = pchar '+' |>> And
+
+let hh: Parser<Drum, unit> = (str_ws0 "hh") |>> (fun x -> HH)
+let sn: Parser<Drum, unit> = (str_ws0 "sn") |>> (fun x -> SN)
+let bd: Parser<Drum, unit> = (str_ws0 "bd") |>> (fun x -> BD)
 
 // type Expr =
 //     | Pattern of VarName * (string list)
 //     | Bar of VarName * ((Drum * Pattern) list)
 
 
-let str s = pstring s
-let str_ws0 s = pstring s .>> ws0
 
 let pattern_keyword = "pattern"
 let bar_keyword = "bar"
 
-let p_valid_word: Parser<_, unit> =
-    many1Satisfy (fun c ->
-        c <> '\\'
-        && c <> '"'
-        && c <> ' '
-        && c <> '\n'
-        && c <> '\r'
-        && c <> '\t')
-
-let p_notes: Parser<_, unit> =
-    many1Satisfy (fun c ->
-        isDigit c
-        || c = 'e'
-        || c = '+'
-        || c = 'a'
-        || c = '|')
-    .>> ws0
-
-
+(*
+    Parses a variable name assignment such as
+    var_pattern_name1:
+*)
 let p_assignment = (manyCharsTill (letter <|> digit) (ws0 >>. str_ws0 ":"))
 
-// "forward declare" <expr>
-// let expr, exprImpl = createParserForwardedToRef ()
+(*
+    Parses a note value such as
+    1, e, and, or a
+*)
+let p_note = (num <|> e <|> and' <|> a)
 
+(*
+    Parses a pattern expression such as
+    pattern var_pattern_name1: 1 2 and a
+*)
 let p_pattern: Parser<Pattern, Unit> =
-    pipe3 (str pattern_keyword .>> ws1) (p_assignment) (many (p_notes)) (fun _ id data -> Pattern(id, data))
+    pipe3 (str_ws1 pattern_keyword) (p_assignment |>> PatternName) (many p_note) (fun _ id data -> Pattern(id, data))
 
-// let p_pattern: Parser<Pattern, Unit> =
-//     pipe3 (str pattern_keyword .>> ws1) (p_assignment) (many (p_notes)) (fun _ id data -> Pattern(id, data))
+(*
+    Parses drum name assignment such as
+    hh:
+*)
+let p_drum = (hh <|> sn <|> bd) .>> (ws0 >>. str_ws0 ":")
 
-let p_drumpattern =
-    pipe2 p_assignment p_pattern (fun drum pattern -> (HH(drum), pattern))
+(*
+    Parses a drum to pattern or pattern variable assignment such as
+    hh: 1 e and a
+    or
+    hh: var_pattern_name1
+*)
+let p_drumpattern_notes =
+    pipe2 p_drum (many p_note) (fun drum pattern -> (drum, pattern))
 
-// let p_bar =
-//     (((str bar_keyword .>> ws1) >>. (p_assignment))
-//      .>>. (many p_drumpattern))
-//     |>> Bar
+let p_drumpattern_var =
+    pipe2 p_drum (manyCharsTill (letter <|> digit) ws1) (fun drum pattern -> (drum, PatternName(pattern)))
 
+
+(*
+    Parses a bar expression such as
+    bar mybar:
+        hh: mypattern
+        sn: 1 2 3 e 4
+*)
 let p_bar: Parser<Bar, Unit> =
-    pipe3 (str bar_keyword .>> ws1) (p_assignment) (many p_drumpattern) (fun _ id data -> Bar(id, data))
+    pipe3
+        (str_ws1 bar_keyword)
+        (p_assignment |>> BarName)
+        (many (p_drumpattern_var .>>. p_drumpattern_notes))
+        (fun _ id data -> Bar(id, data))
+
+
 
 let expr = (many p_pattern .>>. many p_bar) .>> spaces
 
