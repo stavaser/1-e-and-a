7ac9066c9c0443b2a5375b7e6b23ba0f6c35086f
Valeria Starkova <stavaser@icloud.com>
2022-05-01 21:25:15 -0400

    added spec

--- lang/Program.fs
+++ lang/Program.fs
@@ -5,48 +5,25 @@ open FParsec
 open ProjectParser
 open Giraffe.ViewEngine
 
-
-
-
 [<EntryPoint>]
 let main argv =
-    // if argv.Length <> 1 then
-    //     printfn "Usage: dotnet run <program>"
-    //     exit 1
-    // let ast_maybe = parse argv.[0]
-    // match ast_maybe with
-    // | Some ast ->
-    //     let output = eval ast
-    //     use sw = new StreamWriter("output.svg")
-    //     sw.WriteLine(output)
-    // | None     ->
-    //     printfn "Invalid program."
-    //     exit 1
-
     if (Array.length argv <> 1) then
         printfn "usage: dotnet run <input_file>"
-        1
-    else
-        let input = argv.[0]
+        exit 1
 
-        let test p str =
-            match run p str with
-            | Success (result, _, _) ->
-                let output = eval result
-                use sw = new StreamWriter("output.html")
-                sw.WriteLine(output)
-                printfn "Success: %A" (eval result)
-            | Failure (errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+    let input = argv.[0]
+
+    let test p str =
+        match run p str with
+        | Success (result, _, _) ->
+            let output = eval result
+            use sw = new StreamWriter("output.html")
+            sw.WriteLine(output)
+            printfn "Success: %A" (eval result)
+        | Failure (errorMsg, _, _) -> printfn "Failure: %s" errorMsg
 
 
-        let lines = File.ReadAllText input
-        // let list = Seq.toList lines
-        test grammar lines
+    let lines = File.ReadAllText input
+    test grammar lines
 
-        // List.map (fun x -> test parse x) list |> ignore
-
-        // let output = Seq.where (fun (n) -> test p_pattern n) list
-        // printfn "%A" output
-        // test p_pattern input
-
-        0
+    0
--- lang/ProjectInterpreter.fs
+++ lang/ProjectInterpreter.fs
@@ -1,14 +1,6 @@
 module ProjectInterpreter
 
 open ProjectParser
-let doctype = "<?xml version=\"1.0\" standalone=\"no\"?>\n"
-
-let prefix =
-    "<svg width=\"200\" height=\"250\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n"
-
-let suffix = "</svg>\n"
-
-let circle = "<ellipse cx=\"50\" cy=\"50\" rx=\"50\" ry=\"50\" />"
 
 let lines width =
     let prefix =
@@ -40,57 +32,40 @@ let lines width =
     let suffix = "</svg>\n"
     prefix + path + suffix
 
-// let rec evalShapes (ss: Shape list) (x: int) : string =
-//     match ss with
-//     | [] -> ""
-//     | s :: ss' ->
-//         let circle =
-//             "<circle cx=\""
-//             + (string x)
-//             + "\" cy=\"75\" r=\"20\" stroke=\"rgb("
-//             + (string ((x * 2) % 256))
-//             + ",100,100)\" fill=\"transparent\" stroke-width=\"5\" />\n"
 
-//         let circles = evalShapes ss' (x + 30)
-//         circle + circles
+let evalNote note props =
+    match props with
+    | { Time = (x, y); Division = (a, b) } ->
+        match note with
+        | E -> 1
+        | And -> 1
+        | A -> 1
+        | Sep -> 0
+        | Num (n) -> 1
+// | E -> (1.0 / float b)
+// | And -> (1.0 / (float b / 2.0))
+// | A -> (1.0 / float b)
+// | Sep -> 0.0
+// | Num (n) -> 1.0
 
-// let eval2 (e: Expr) : string =
-//     let str =
-//         match e with
-//         | Shapes ss -> evalShapes ss 25
-
-//     doctype + prefix + str + suffix
-
-let evalNote note =
-    match note with
-    | E -> 2
-    | And -> 3
-    | A -> 4
-    | num -> 1
 // | E -> "<ellipse cx=\"50\" cy=\"50\" rx=\"50\" ry=\"50\" />"
 // | And -> "<ellipse cx=\"50\" cy=\"50\" rx=\"50\" ry=\"50\" />"
 // | A -> "<ellipse cx=\"50\" cy=\"50\" rx=\"50\" ry=\"50\" />"
 // | num -> "<ellipse cx=\"50\" cy=\"50\" rx=\"50\" ry=\"50\" />"
 
-// let evalNotes notes =
-//     match notes with
-//     | [] -> ""
-//     | note :: rest -> evalNote note
 
-let evalCanvas data =
+let evalBar pattern data =
     match data with
     | { Time = (x, y); Division = (a, b) } ->
         let beats = int (b / y)
         let bar = [| for i in 1 .. (int x) -> [| for i in 1 .. (beats) -> 0 |] |]
-        bar
-// String.replicate (int b) (lines 102)
+        let width = (beats * (int y)) * 50
+        (lines width)
+// String.replicate (int b) (lines 300)
 
 
-// let evalPatterns patterns =
-//     match patterns with
-//     | [] -> 0
-//     // | Pattern (varname, notes) -> varname
-//     | Pattern (varname, notes) :: rest -> evalNotes notes
+let evalPattern pattern props =
+    List.map (fun x -> (evalNote x props)) pattern
 
 
 let evalSettings settings = settings
@@ -104,8 +79,9 @@ let findExpr exprs variable =
 
 let eval e =
     match e with
-    | { Settings = s_data
+    | { Settings = props
         Patterns = p_data
-        Render = varname } -> (findExpr p_data varname)
-// (evalCanvas s_data)
-// evalSettings s_data
+        Render = varname } ->
+        let expr = (findExpr p_data varname)
+        let pattern = (fun (Pattern (_, pattern)) -> evalPattern pattern props) expr
+        evalBar pattern props
