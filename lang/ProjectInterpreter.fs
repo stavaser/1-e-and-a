a35e9e19c88bb598500a9ce68a39850f0e1df511
Valeria Starkova <stavaser@icloud.com>
2022-05-06 16:19:45 -0400

    correct spacing of notes

--- lang/ProjectInterpreter.fs
+++ lang/ProjectInterpreter.fs
@@ -4,6 +4,8 @@ open System
 
 open ProjectParser
 
+let MAX_LINE_WIDTH = 681.6
+
 let TIME_SIG (a, b) =
     "("
     + string a
@@ -17,57 +19,60 @@ let DRUM_BASS distance = string distance + " bass\n"
 
 
 // [100.0; 112.5; 125.0; 137.5]
-let rec evalOneBeat (pattern: Note list) (numBeats: int) div distance prevVal =
-    let multuplier current prev =
-        match current with
-        | E -> 1.0
-        | And ->
-            match prev with
-            | Num (_) -> 2.0
+let evalOneBeat beat numBeats div i =
+    let rec evalOneBeatHelper (pattern: Note list) (numBeats: int) div distance prevVal =
+        let multuplier current prev =
+            match current with
             | E -> 1.0
-            | _ -> failwith ("Incorrect position of notes: '+' cannot come after 'a'")
-        | A ->
-            match prev with
-            | Num (n) -> 3.0
-            | E -> 2.0
-            | And -> 1.0
-            | _ -> failwith ("Incorrect position of notes: 'a'")
-        | a -> failwith ("Something went wrong: " + string a)
+            | And ->
+                match prev with
+                | Num (_) -> 2.0
+                | E -> 1.0
+                | _ -> failwith ("Incorrect position of notes: '+' cannot come after 'a'")
+            | A ->
+                match prev with
+                | Num (n) -> 3.0
+                | E -> 2.0
+                | And -> 1.0
+                | _ -> failwith ("Incorrect position of notes: 'a'")
+            | a -> failwith ("Something went wrong: " + string a)
 
-    match pattern with
-    | [] -> []
-    | head :: tail ->
-        match head with
-        // the first note of the beat
-        | Num (n) ->
-            if int n <= numBeats then
-                // calculate the distance of the next note
-                distance
-                :: (evalOneBeat tail numBeats div distance (Num(n)))
-            else
-                failwith ("Number of beats exceeds " + string numBeats + ".")
-        | E ->
-            let new_dist = (distance + (div * (multuplier E prevVal) * 100.0))
+        match pattern with
+        | [] -> []
+        // [n, e, and, a]
+        | head :: tail ->
+            match head with
+            // the first note of the beat
+            // 4/4
+            | Num (n) ->
+                if int n <= numBeats then
+                    // calculate the distance of the next note
+                    distance
+                    :: (evalOneBeatHelper tail numBeats div distance (Num(n)))
+                else
+                    failwith ("Number of beats exceeds " + string numBeats + ".")
+            | E ->
+                let new_dist = (distance + (div * (multuplier E prevVal)))
 
-            new_dist
-            :: (evalOneBeat tail numBeats div new_dist E)
+                new_dist
+                :: (evalOneBeatHelper tail numBeats div new_dist E)
+            // 1/16
+            | And ->
+                let new_dist = (distance + (div * (multuplier And prevVal)))
 
-        | And ->
-            let new_dist =
-                (distance
-                 + (div * (multuplier And prevVal) * 100.0))
+                new_dist
+                :: (evalOneBeatHelper tail numBeats div new_dist And)
 
-            new_dist
-            :: (evalOneBeat tail numBeats div new_dist And)
+            | A ->
+                let new_dist = (distance + (div * (multuplier A prevVal)))
 
-        | A ->
-            let new_dist = (distance + (div * (multuplier A prevVal) * 100.0))
+                new_dist
+                :: (evalOneBeatHelper tail numBeats div new_dist A)
 
-            new_dist
-            :: (evalOneBeat tail numBeats div new_dist A)
+    evalOneBeatHelper beat numBeats div i (Num(uint8 1))
 
 
-let separatePattern pattern beat =
+let separatePattern pattern =
     let rec separatePatternHelper pattern beat =
         match pattern with
         | [] -> []
@@ -76,17 +81,21 @@ let separatePattern pattern beat =
             | Sep -> (beat :: separatePatternHelper tail [])
             | _ -> separatePatternHelper tail (head :: beat)
 
-    let list = separatePatternHelper pattern beat
+    let list = separatePatternHelper pattern []
     list |> List.map List.rev
 
 let rec evalOnePattern pattern numBeats div =
-    let separatedPattern = separatePattern pattern []
+    let separatedPattern = separatePattern pattern
+    // where the first note of a beat should start
+    let note_start = MAX_LINE_WIDTH / float numBeats
+    // minimum distance between two consequtive notes
+    let dist = note_start / float numBeats
 
-    List.mapi (fun i x -> evalOneBeat x numBeats div (100.0 * (float (i + 1))) (Num(uint8 1))) separatedPattern
+    List.mapi (fun i x -> evalOneBeat x numBeats dist ((float i) * note_start)) separatedPattern
 
 let createPatternPS pattern numBeats div =
+    // 2d list of distances of notes in each beat
     let distances = evalOnePattern pattern numBeats div
-
 
     List.map
         (fun one_beat ->
@@ -117,7 +126,7 @@ let eval
     =
     // get values of the settings
     let ((numBeats, beatValue), div) =
-        (fun { Time = (a, b); Division = (_, y) } -> (int a, int b), 1.0 / float y) settings
+        (fun { Time = (a, b); Division = (_, y) } -> (int a, int b), float y) settings
 
     let envPattern =
         patterns
@@ -129,7 +138,9 @@ let eval
         // separatePattern pattern []
         let drums = createPatternPS pattern numBeats div
 
-        LINE(400) + TIME_SIG(numBeats, beatValue) + drums
+        LINE(MAX_LINE_WIDTH)
+        + TIME_SIG(numBeats, beatValue)
+        + drums
     // evalOneBeat pattern numBeats div 100.0 (Num(uint8 1))
     else
         failwith ("Undefined variable '" + render + "'")
