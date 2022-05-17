9374077ac7c4a2fe53ae813274cc5e9a735dfd58
Valeria Starkova <stavaser@icloud.com>
2022-05-16 22:00:13 -0400

    test?

--- ParserTests/Tests.fs
+++ ParserTests/Tests.fs
@@ -37,141 +37,75 @@ type TestClass() =
             | Success (result, _, _) -> eval result
             | Failure (errorMsg, _, _) -> "error"
 
-        let actual = (test grammar lines).Trim([ ' ' ])
+        let actual = (test grammar lines)
 
         let expected =
             "340.8 4.0 M  20.0 F2 (helo)showc
-                0 -19.00 T 
-            340.8 3.2 M  16.0 F2 (new)showc
-                0 -12.00 T
-            681.6 newline
-            (4)(4) 30.5 -47.0 tsig
-            60 crash
+    0 -19.00 T 
+340.8 3.2 M  16.0 F2 (new)showc
+    0 -12.00 T
+681.6 newline
+(4)(4) 30.5 -47.0 tsig
+60 crash
 
-            137.7 crash
+137.7 crash
 
-            215.4 crash
+215.4 crash
 
-            293.1 crash
+293.1 crash
 
-            370.8 crash
+370.8 crash
 
-            448.5 crash
+448.5 crash
 
-            526.2 crash
+526.2 crash
 
-            603.9000000000001 crash
+603.9000000000001 crash
 
-            60 closedhh
+60 closedhh
 
-            137.7 closedhh
+137.7 closedhh
 
-            215.4 closedhh
+215.4 closedhh
 
-            293.1 closedhh
+293.1 closedhh
 
-            370.8 closedhh
+370.8 closedhh
 
-            448.5 closedhh
+448.5 closedhh
 
-            526.2 closedhh
+526.2 closedhh
 
-            603.9000000000001 closedhh
+603.9000000000001 closedhh
 
-            60 snare
+60 snare
 
-            98.85 snare
+98.85 snare
 
-            137.7 snare
+137.7 snare
 
-            176.54999999999998 snare
+176.54999999999998 snare
 
-            215.4 snare
+215.4 snare
 
-            370.8 snare
+370.8 snare
 
-            526.2 snare
+526.2 snare
 
-            60 bass
+60 bass
 
-            215.4 bass
+215.4 bass
 
-            370.8 bass
+370.8 bass
 
-            526.2 bass
+526.2 bass
 
-            565.0500000000001 bass
+565.0500000000001 bass
 
-            603.9000000000001 bass
+603.9000000000001 bass
 
-            642.7500000000001 bass
-            >. Actual:<340.8 4.0 M  20.0 F2 (helo)showc
-                0 -19.00 T 
-            340.8 3.2 M  16.0 F2 (new)showc
-                0 -12.00 T
-            681.6 newline
-            (4)(4) 30.5 -47.0 tsig
-            60 crash
-
-            137.7 crash
-
-            215.4 crash
-
-            293.1 crash
-
-            370.8 crash
-
-            448.5 crash
-
-            526.2 crash
-
-            603.9000000000001 crash
-
-            60 closedhh
-
-            137.7 closedhh
-
-            215.4 closedhh
-
-            293.1 closedhh
-
-            370.8 closedhh
-
-            448.5 closedhh
-
-            526.2 closedhh
-
-            603.9000000000001 closedhh
-
-            60 snare
-
-            98.85 snare
-
-            137.7 snare
-
-            176.54999999999998 snare
-
-            215.4 snare
-
-            370.8 snare
-
-            526.2 snare
-
-            60 bass
-
-            215.4 bass
-
-            370.8 bass
-
-            526.2 bass
-
-            565.0500000000001 bass
-
-            603.9000000000001 bass
-
-            642.7500000000001 bass
-            "
-                .Trim([ ' ' ])
+642.7500000000001 bass
+"
 
         // My evaluated output is a file. I couldn't figure out how to open a file in Test.fs
         // this method I am trying to do doesn't work.
